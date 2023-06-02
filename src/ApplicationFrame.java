94ca345cce83a5dcde2456255622eeb0190a64d9
soulasphyxia <maslyuk03@bk.ru>
2023-06-02 22:51:08 +0700

    initial commit

---
+++ src/ApplicationFrame.java
@@ -0,0 +1,18 @@
+import javax.swing.*;
+
+
+public class ApplicationFrame extends JFrame{
+    public ApplicationFrame() {
+        super("Air Company");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setSize(500, 500);
+        setLocationRelativeTo(null);
+        setLayout(null);
+        setVisible(true);
+    }
+
+
+    private void initComponents() {
+
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,18 @@
+import javax.swing.*;
+import java.lang.reflect.InvocationTargetException;
+
 public class Main {
-    public static void main(String[] args) {
-        AirCompany airCompany = AircompanyFactory.createAirCompany();
-        System.out.println("Общая вместимость авиакомпании: " + airCompany.getTotalCapacity());
+    public static void main(String[] args) throws InterruptedException, InvocationTargetException {
+        Runnable initFrame = new initFrame();
+        SwingUtilities.invokeAndWait(initFrame);
     }
+
+
+    public static class initFrame implements Runnable{
+        @Override
+        public void run() {
+            new ApplicationFrame();
+        }
+    }
+
 }
